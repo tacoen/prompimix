82f60d43c545869d94c22a9dd9118eacf5366157
tacoen <tacoen@gmail.com>
2024-01-29 00:10:21 +0900

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function fbutton(obj) {
+	var url = obj.getAttribute('href');
+	window.location.href = url;
+}
+
+const pic = [
+	['pic/1.jpg','batik, playful pose, strong facial expression, pirate ship, beautiful morning sunlight, day glo, intense emotional atmosphere, canon ef 16-35mm f/2.8l iii usm lens','leonardo.ai'],
+	['pic/2.jpg','3d render, a girl wearing animal-costume, serene pose in amusement park, style of akira toriyama','leonardo.ai'],
+	['pic/3.jpg','photo, medium shot, a pretty girl wearing wide sleeveless animal-costume, serene pose in amusement park, side view, posing with one-arm raised, photo by ellen von unwerth, glimmering light effects','leonardo.ai'],
+	['pic/4.jpg','photo, medium shot, male in red mobile-suit, serene pose in outer space, front view, action pose, foreboding emotional tone, photo photo by steve mccurry','leonardo.ai'],
+	['pic/5.jpg','anime with insane details, a pretty girl wearing wide-sleeveless animal-costume, serene pose in amusement park, side view, posing with one-arm raised, photo by ellen von unwerth, glimmering light effects','leonardo.ai'],
+	['pic/6.jpg','medium shot, afrofuturism, ross tran style, girl wearing batik, playful pose, strong facial expression, pirate ship, beautiful morning sunlight, day glo, intense emotional atmosphere, canon ef 16-35mm f/2.8l iii usm lens','leonardo.ai'],
+]
